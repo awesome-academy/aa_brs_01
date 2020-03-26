50d3d1a303b08473aef045b33e96b0daeb85c742
Doan Thu Van <doanthuvan8698@gmail.com>
2020-03-26 11:59:05 +0700

    *

---
+++ resources/views/user/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<!-- footer-area-start -->
+<footer>
+    <!-- footer-top-start -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-top-menu bb-2">
+                        <nav>
+                            <ul>
+                                <li><a href="#">home</a></li>
+                                <li><a href="#">Enable Cookies</a></li>
+                                <li><a href="#">Privacy and Cookie Policy</a></li>
+                                <li><a href="#">contact us</a></li>
+                                <li><a href="#">blog</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer-top-start -->
+    
